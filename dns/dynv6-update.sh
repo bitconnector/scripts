242c6c2ff68d2650c17221ad8d27fe4665b5a232
Lukas <luk-krat@web.de>
2021-11-16 14:28:31 +0100

    improved dynv6 script for continous running

--- dns/dynv6-update.sh
+++ dns/dynv6-update.sh
@@ -1,27 +1,31 @@
 #!/bin/bash
 TOKEN="ke5gDKehuikdjDEnekSWO2Di23Kid9"
 HOSTNAME="MYSERVER.dynv6.net"
+SLEEP=30
 
-FILE=$HOME/.dynv6.addr6
+OLD=""
 
-ADDRESS=$(ip -6 addr list scope global $device | grep -v " fd" | sed -n 's/.*inet6 \([0-9a-f:]\+\).*/\1/p' | head -n 1)
-
-[ -e $FILE ] && old=$(cat $FILE)
-
-if [ -z "$ADDRESS" ]; then
-  echo "no IPv6 address found"
-  exit 1
-fi
-
-# address with netmask
-CURRENT=$ADDRESS/128
-
-if [ "$old" = "$current" ]; then
-  #echo "IPv6 address unchanged"
-  exit
-fi
-
-wget -O- "https://dynv6.com/api/update?hostname=$HOSTNAME&ipv6=$CURRENT&token=$TOKEN"
-#curl -fsS "http://dynv6.com/api/update?hostname=$HOSTNAME&ipv6=$CURRENT&token=$TOKEN"
-
-echo $current >$FILE
+while true
+do
+    ADDRESS=$(ip -6 addr list scope global $device | grep -v " fd" | sed -n 's/.*inet6 \([0-9a-f:]\+\).*/\1/p' | head -n 1)
+    
+    if [ -z "$ADDRESS" ]; then
+        echo "no IPv6 address found"
+        exit 1
+    fi
+    
+    # address with netmask
+    CURRENT=$ADDRESS/128
+    
+    if [ "$OLD" != "$CURRENT" ]; then
+        echo "IPv6 address changed: $CURRENT"
+        wget -O- -q "https://dynv6.com/api/update?hostname=$HOSTNAME&ipv6=$CURRENT&token=$TOKEN" 2> /dev/null
+        #curl -fsS "http://dynv6.com/api/update?hostname=$HOSTNAME&ipv6=$CURRENT&token=$TOKEN"
+        
+        echo ""
+        
+        OLD=$CURRENT
+    fi
+    
+    sleep $SLEEP
+done
