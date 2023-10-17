576a5c9cf6d54dcf8c5b8fc2a99e7b8ba3c13c1a
bitconnector <luk-krat@web.de>
2023-10-17 15:46:36 +0200

    format document

--- music/youtube-music.sh
+++ music/youtube-music.sh
@@ -4,8 +4,8 @@
 #"$@"
 
 if [ $# -eq 0 ]; then
-  echo "usage: youtube-music VIDEO-ID [TITEL KÜNSTLER]"
-  exit
+    echo "usage: youtube-music VIDEO-ID [TITEL KÜNSTLER]"
+    exit
 fi
 
 id=$1
@@ -16,23 +16,23 @@ mkdir -p $tmpdir
 #echo "$id = ${#id}"
 
 if [ ${#id} -eq 11 ]; then
-  if [ ! $(find ./ "$tmpdir" -name \*"$id"\* | wc -l) -eq 0 ]; then
-    if [ ! "$(find "$tmpdir" -name \*"$id"\* | wc -l)" -eq 0 ]; then
-      echo "still in progress ... $id"
-    else
-      echo "$(find ./ -name \*"$id"\*)"
-      echo "already downloaded."
+    if [ ! $(find ./ "$tmpdir" -name \*"$id"\* | wc -l) -eq 0 ]; then
+        if [ ! "$(find "$tmpdir" -name \*"$id"\* | wc -l)" -eq 0 ]; then
+            echo "still in progress ... $id"
+        else
+            echo "$(find ./ -name \*"$id"\*)"
+            echo "already downloaded."
+        fi
+        exit 0
     fi
-    exit 0
-  fi
 else
-  echo "No ID supplied fallback"
-  fileName=$(yt-dlp --get-filename $id --restrict-filenames)
-  fileName=${fileName%.*}
-  if [ ! $(find ./ -name "$fileName.mp3" | wc -l) -eq 0 ]; then
-    echo "already downloaded. Found file $fileName.mp3"
-    exit 1
-  fi
+    echo "No ID supplied fallback"
+    fileName=$(yt-dlp --get-filename $id --restrict-filenames)
+    fileName=${fileName%.*}
+    if [ ! $(find ./ -name "$fileName.mp3" | wc -l) -eq 0 ]; then
+        echo "already downloaded. Found file $fileName.mp3"
+        exit 1
+    fi
 fi
 
 fileName=$(yt-dlp --get-filename $id --restrict-filenames)
@@ -42,28 +42,28 @@ printf -v fileNameE "%q" "$fileName"
 year=$(date +"%Y")
 #year="test"
 if [[ ! -d "$year" ]]; then
-  echo "creating $year"
-  mkdir $year
+    echo "creating $year"
+    mkdir $year
 fi
 
 if [ $# -eq 3 ]; then
-  echo "Tags supplied"
-  title=$2
-  artist=$3
-  echo "Titel: $title"
-  echo "Künstler: $artist"
-
+    echo "Tags supplied"
+    title=$2
+    artist=$3
+    echo "Titel: $title"
+    echo "Künstler: $artist"
+    
 else
-  ## get infos
-  echo "Tags have to be supplied manually"
-  videoTitle=$(yt-dlp --get-title $id)
-  uploader=$(yt-dlp --get-filename -o '%(uploader)s' $id --restrict-filenames)
-
-  echo "$videoTitle"
-  echo "$uploader"
-  read -p "Title: " title
-  read -p "Artist: " artist
-
+    ## get infos
+    echo "Tags have to be supplied manually"
+    videoTitle=$(yt-dlp --get-title $id)
+    uploader=$(yt-dlp --get-filename -o '%(uploader)s' $id --restrict-filenames)
+    
+    echo "$videoTitle"
+    echo "$uploader"
+    read -p "Title: " title
+    read -p "Artist: " artist
+    
 fi
 
 echo "DOWNLOAD"
@@ -84,7 +84,7 @@ eyeD3 --quiet --no-color --artist="$artist" --title="$title" "$year/$fileName.mp
 image=$(find $tmpdir -name "$fileNameE.*" -type f -exec file --mime-type {} \+ | awk -F: '{if ($2 ~/image\//) print $1}')
 convert $image -fuzz 5% -trim +repage $image
 if [ ! -f $tmpdir/$fileName.png ]; then
-  ffmpeg -y -hide_banner -loglevel warning -i $image $tmpdir/$fileNameE.jpg
+    ffmpeg -y -hide_banner -loglevel warning -i $image $tmpdir/$fileNameE.jpg
 fi
 mogrify -background White -alpha remove -define jpeg:extent=100KB $tmpdir/$fileName.jpg
 
