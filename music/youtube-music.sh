ac1270e30768ca24186ee4716e9a8fad88bbd524
bitconnector <luk-krat@web.de>
2023-10-17 15:46:02 +0200

    update youtube-dl -> yt-dlp

--- music/youtube-music.sh
+++ music/youtube-music.sh
@@ -27,7 +27,7 @@ if [ ${#id} -eq 11 ]; then
   fi
 else
   echo "No ID supplied fallback"
-  fileName=$(youtube-dl --get-filename $id --restrict-filenames)
+  fileName=$(yt-dlp --get-filename $id --restrict-filenames)
   fileName=${fileName%.*}
   if [ ! $(find ./ -name "$fileName.mp3" | wc -l) -eq 0 ]; then
     echo "already downloaded. Found file $fileName.mp3"
@@ -35,8 +35,9 @@ else
   fi
 fi
 
-fileName=$(youtube-dl --get-filename $id --restrict-filenames)
+fileName=$(yt-dlp --get-filename $id --restrict-filenames)
 fileName=${fileName%.*}
+printf -v fileNameE "%q" "$fileName"
 
 year=$(date +"%Y")
 #year="test"
@@ -55,8 +56,8 @@ if [ $# -eq 3 ]; then
 else
   ## get infos
   echo "Tags have to be supplied manually"
-  videoTitle=$(youtube-dl --get-title $id)
-  uploader=$(youtube-dl --get-filename -o '%(uploader)s' $id --restrict-filenames)
+  videoTitle=$(yt-dlp --get-title $id)
+  uploader=$(yt-dlp --get-filename -o '%(uploader)s' $id --restrict-filenames)
 
   echo "$videoTitle"
   echo "$uploader"
@@ -66,24 +67,29 @@ else
 fi
 
 echo "DOWNLOAD"
-youtube-dl -w --extract-audio --write-thumbnail --restrict-filenames -o "$tmpdir/$fileName.opus" $id
+yt-dlp -w --extract-audio --write-thumbnail --restrict-filenames -o "$tmpdir/$fileName" $id
 
+echo "fileName $fileName"
 echo "NORMALIZE"
 
-audio=$(find $tmpdir -name "$fileName.*" -type f -exec file --mime-type {} \+ | awk -F: '{if ($2 ~ /video|audio/) print $1}')
+audio=$(find $tmpdir -name "$fileNameE.*" -type f -exec file --mime-type {} \+ | awk -F: '{if ($2 ~ /video|audio/) print $1}')
+
+echo "Audio $audio"
 ffmpeg-normalize $audio -nt rms --target-level -14 -c:a libmp3lame -o "$year/$fileName.mp3"
 
 echo "TAGS"
 
 eyeD3 --quiet --no-color --artist="$artist" --title="$title" "$year/$fileName.mp3"
 
-image=$(find $tmpdir -name "$fileName.*" -type f -exec file --mime-type {} \+ | awk -F: '{if ($2 ~/image\//) print $1}')
+image=$(find $tmpdir -name "$fileNameE.*" -type f -exec file --mime-type {} \+ | awk -F: '{if ($2 ~/image\//) print $1}')
 convert $image -fuzz 5% -trim +repage $image
 if [ ! -f $tmpdir/$fileName.png ]; then
-  ffmpeg -y -hide_banner -loglevel warning -i $image $tmpdir/$fileName.jpg
+  ffmpeg -y -hide_banner -loglevel warning -i $image $tmpdir/$fileNameE.jpg
 fi
 mogrify -background White -alpha remove -define jpeg:extent=100KB $tmpdir/$fileName.jpg
 
-eyeD3 --quiet --no-color --add-image "$tmpdir/$fileName.jpg:FRONT_COVER:$fileName.jpg" "$year/$fileName.mp3"
+eyeD3 --quiet --no-color --add-image "$tmpdir/$fileNameE.jpg:FRONT_COVER:$fileName.jpg" "$year/$fileName.mp3"
 
-rm $tmpdir/$fileName.*
+find $tmpdir -name "*$fileNameE*" -type f
+
+#rm $tmpdir/$fileName.*
